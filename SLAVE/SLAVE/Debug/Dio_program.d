976f789d3d4fdb06dea0e933aa14d02d93a64f4d
donyamohammed <58361548+donyamohammed@users.noreply.github.com>
2021-09-06 23:21:08 +0200

    Add files via upload

---
+++ SLAVE/SLAVE/Debug/Dio_program.d
@@ -0,0 +1,10 @@
+Dio_program.d Dio_program.o: .././Dio_program.c .././STD_TYPES.h \
+ .././BIT_MATH.h .././Dio_interface.h .././Dio_private.h
+
+.././STD_TYPES.h:
+
+.././BIT_MATH.h:
+
+.././Dio_interface.h:
+
+.././Dio_private.h:
